e248e504e2014e6330f81549575cc9bae3eabfb3
James Talton <jtalton@redhat.com>
2022-06-30 11:39:02 -0400

    breadcrumb routing
    Signed-off-by: James Talton <jtalton@redhat.com>

--- src/PageHeader.tsx
+++ src/PageHeader.tsx
@@ -1,5 +1,6 @@
 import { Breadcrumb, BreadcrumbItem, PageSection, Stack, Text, Title } from '@patternfly/react-core'
-import { Fragment, useMemo } from 'react'
+import { Fragment } from 'react'
+import { useHistory } from 'react-router-dom'
 import { useWindowSizeOrLarger, WindowSize } from './components/useBreakPoint'
 
 export interface ICatalogBreadcrumb {
@@ -10,51 +11,50 @@ export interface ICatalogBreadcrumb {
     component?: React.ElementType
 }
 
-export function PageHeader(props: { breadcrumbs?: ICatalogBreadcrumb[]; title?: string; description?: string }) {
-    const { breadcrumbs: breadcrumb, title, description } = props
-    const isXlOrLarger = useWindowSizeOrLarger(WindowSize.xl)
-
-    const breadcrumbs = useMemo(() => {
-        if (!props.breadcrumbs) return <Fragment />
-        return (
-            <Breadcrumb>
-                {props.breadcrumbs.map((breadcrumb) => (
-                    <BreadcrumbItem
-                        id={breadcrumb.id}
-                        key={breadcrumb.id ?? breadcrumb.label}
-                        to={breadcrumb.to}
-                        target={breadcrumb.target}
-                        component={breadcrumb.component}
-                    >
-                        {breadcrumb.label}
-                    </BreadcrumbItem>
-                ))}
-            </Breadcrumb>
-        )
-    }, [props.breadcrumbs])
-
-    const header = useMemo(
-        () => (
-            <Fragment>
-                {(title || breadcrumb) && (
-                    <PageSection variant="light" style={{ paddingBottom: 0 }}>
-                        <Stack>
-                            {breadcrumb !== undefined && (
-                                <div style={{ paddingBottom: isXlOrLarger ? 8 : 6, marginTop: -6 }}>{breadcrumbs}</div>
-                            )}
-                            {title && <Title headingLevel="h2">{title}</Title>}
-                            {description && (
-                                <Text component="p" style={{ opacity: 0.9 }}>
-                                    {description}
-                                </Text>
-                            )}
-                        </Stack>
-                    </PageSection>
-                )}
-            </Fragment>
-        ),
-        [breadcrumb, breadcrumbs, description, isXlOrLarger, title]
+function Breadcrumbs(props: { breadcrumbs: ICatalogBreadcrumb[] }) {
+    const history = useHistory()
+    if (!props.breadcrumbs) return <Fragment />
+    return (
+        <Breadcrumb>
+            {props.breadcrumbs.map((breadcrumb) => (
+                <BreadcrumbItem
+                    id={breadcrumb.id}
+                    key={breadcrumb.id ?? breadcrumb.label}
+                    component={breadcrumb.component}
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    onClick={breadcrumb.to ? () => history.push(breadcrumb.to!) : undefined}
+                    style={{ color: breadcrumb.to ? 'var(--pf-c-breadcrumb__link--Color)' : undefined }}
+                    isActive={breadcrumb.to === undefined}
+                >
+                    {breadcrumb.label}
+                </BreadcrumbItem>
+            ))}
+        </Breadcrumb>
     )
+}
 
-    return header
+export function PageHeader(props: { breadcrumbs?: ICatalogBreadcrumb[]; title?: string; description?: string }) {
+    const { breadcrumbs, title, description } = props
+    const isXlOrLarger = useWindowSizeOrLarger(WindowSize.xl)
+    return (
+        <Fragment>
+            {(title || breadcrumbs) && (
+                <PageSection variant="light" style={{ paddingBottom: 0 }}>
+                    <Stack>
+                        {breadcrumbs !== undefined && (
+                            <div style={{ paddingBottom: isXlOrLarger ? 8 : 6, marginTop: -6 }}>
+                                <Breadcrumbs breadcrumbs={breadcrumbs} />
+                            </div>
+                        )}
+                        {title && <Title headingLevel="h2">{title}</Title>}
+                        {description && (
+                            <Text component="p" style={{ opacity: 0.9 }}>
+                                {description}
+                            </Text>
+                        )}
+                    </Stack>
+                </PageSection>
+            )}
+        </Fragment>
+    )
 }
